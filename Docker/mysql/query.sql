a894d3f60d4745ce829a69b759493e4cd79b0f0c
Bhavin Bhesaniya <66940770+Bhavin-Bhesaniya@users.noreply.github.com>
2023-10-08 10:38:48 +0530

    Update query and env

--- Docker/mysql/query.sql
+++ Docker/mysql/query.sql
@@ -2,3 +2,9 @@ CREATE DATABASE IF NOT EXISTS 'spam_mail_detection_db';
 GO
 USE 'spam_mail_detection_db';
 GO
+CREATE USER 'bhavin'@'localhost' IDENTIFIED BY 'SpamMysql@1234.';
+GO
+GRANT ALL PRIVILEGES ON spam_mail_detection_db.* TO 'bhavin'@'localhost';
+GO
+FLUSH PRIVILEGES;
+GO
