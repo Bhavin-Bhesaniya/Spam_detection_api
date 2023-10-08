b524c6124c087cab17f79b478af7b30d21b7f971
Bhavin Bhesaniya <66940770+Bhavin-Bhesaniya@users.noreply.github.com>
2023-10-08 12:40:30 +0530

    Push Debug

--- spam_mail_project/settings.py
+++ spam_mail_project/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = bool(os.environ.get("DEBUG", default=0))
+DEBUG = False
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split()
 
 INSTALLED_APPS = [
