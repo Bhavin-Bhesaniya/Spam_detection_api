99485f790217bb9e115afb1c9a641c27e73decf0
Bhavin Bhesaniya <66940770+Bhavin-Bhesaniya@users.noreply.github.com>
2023-10-15 13:50:58 +0530

    No static

--- spam_mail_project/settings.py
+++ spam_mail_project/settings.py
@@ -86,10 +86,6 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static'),
-    os.path.join(BASE_DIR, '/app/static'),
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
