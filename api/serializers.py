0b643dcac5d6a554b1aa4d5d7b65f5c05add92fc
Bhavin Bhesaniya <66940770+Bhavin-Bhesaniya@users.noreply.github.com>
2023-12-30 13:03:26 +0530

    Commit

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,5 @@
 from rest_framework import serializers
 from .models import MyUser
-from django.utils.encoding import force_bytes, smart_str, DjangoUnicodeDecodeError
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-
 
 class SpamClassifierSerializer(serializers.Serializer):
     user_message = serializers.CharField()
